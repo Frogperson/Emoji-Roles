98177233d263e74da7df330643b22559682eec08
Frogperson <frgprsn@gmail.com>
2018-06-26 12:48:25 -0400

    Only reacting with emoji that are in the body of the Role Message will modify the users role.

--- src/main/java/org/frogperson/emojiroles/ReactionListener.java
+++ src/main/java/org/frogperson/emojiroles/ReactionListener.java
@@ -17,13 +17,10 @@ import static org.frogperson.emojiroles.EmojiRoles.refreshBotActive;
 
 public class ReactionListener extends ListenerAdapter {
 
-    Timer timer= new Timer();
+    Timer timer = new Timer();
 
     @Override
     public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
-        String roleName;
-        Role role;
-        String roleId;
         GuildController guildController = new GuildController(jda.getGuildById(event.getGuild().getId()));
         int delay = Settings.getRefreshTimer();
         boolean isRefreshBotReaction = false;
@@ -32,61 +29,64 @@ public class ReactionListener extends ListenerAdapter {
         if (refreshBotActive)
             isRefreshBotReaction = event.getMember().getUser().getId().equals(jda1.getSelfUser().getId());
 
+        //Message is role message, reaction is not from this bot or refresh bot
         if (JsonDatabase.isMessageRoleMessage(event.getMessageId()) && !event.getMember().getUser().getId().equals(jda.getSelfUser().getId()) && !isRefreshBotReaction) {
             event.getReaction().removeReaction(event.getUser()).complete();
-            if (!refreshBotActive) { //Don't do this if we have another bot reacting
-                timer.cancel();
-                timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        event.getChannel().getMessageById(event.getMessageId()).queue((Message message) -> {
-                            message.clearReactions().complete();
-                            for (Emote emoji : message.getEmotes()) {
-                                if (JsonDatabase.getLinkedRoleFromEmoji(emoji.getId()) != null)
-                                    event.getChannel().addReactionById(event.getMessageId(), emoji).queue();
+
+            event.getChannel().getMessageById(event.getMessageId()).queue((Message message) -> {
+                List<Emote> emojiList = new ArrayList<>();
+                for (Emote emoji : message.getEmotes()) {
+                    if (JsonDatabase.getLinkedRoleFromEmoji(emoji.getId()) != null)
+                        emojiList.add(emoji);
+                }
+
+                //Only do things if the reacted emoji is in the Role Message
+                if (emojiList.contains(event.getReaction().getReactionEmote().getEmote())) {
+                    System.out.println("test");
+                    if (!refreshBotActive) { //Don't do this if we have another bot reacting
+                        timer.cancel();
+                        timer = new Timer();
+                        timer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                event.getChannel().getMessageById(event.getMessageId()).queue((Message message) -> {
+                                    message.clearReactions().complete();
+                                    for (Emote emoji : emojiList) {
+                                        event.getChannel().addReactionById(event.getMessageId(), emoji).queue();
+                                    }
+                                });
                             }
-                        });
+                        }, delay * 1000);
                     }
-                }, delay * 1000);
-            }
 
-            try {
-                roleId = JsonDatabase.getLinkedRoleFromEmoji(event.getReactionEmote().getId());
-                roleName = jda.getRoleById(roleId).getName();
-                role = jda.getRoleById(roleId);
+                    try {
+                        String roleId = JsonDatabase.getLinkedRoleFromEmoji(event.getReactionEmote().getId());
+                        String roleName = jda.getRoleById(roleId).getName();
+                        Role role = jda.getRoleById(roleId);
 
-                //Add the role to the member
-                if (!Commands.doesMemberHaveRole(event.getMember(), roleName)) {
-                    guildController.addSingleRoleToMember(event.getMember(), role).queue();
-                    System.out.println("Gave " + role.getName() + " to " + event.getMember().getUser().getName());
-                    if (!Settings.getRoleAddedEmoji().equals("")) {
-                        event.getChannel().addReactionById(event.getMessageId(), jda.getEmoteById(Settings.getRoleAddedEmoji())).complete();
-                        event.getChannel().removeReactionById(event.getMessageId(), jda.getEmoteById(Settings.getRoleAddedEmoji())).queueAfter(3, TimeUnit.SECONDS);
+                        //Add the role to the member
+                        if (!Commands.doesMemberHaveRole(event.getMember(), roleName)) {
+                            guildController.addSingleRoleToMember(event.getMember(), role).queue();
+                            System.out.println("Gave " + role.getName() + " to " + event.getMember().getUser().getName());
+                            if (!Settings.getRoleAddedEmoji().equals("")) {
+                                event.getChannel().addReactionById(event.getMessageId(), jda.getEmoteById(Settings.getRoleAddedEmoji())).complete();
+                                event.getChannel().removeReactionById(event.getMessageId(), jda.getEmoteById(Settings.getRoleAddedEmoji())).queueAfter(3, TimeUnit.SECONDS);
+                            }
+                        }
+                        //Remove the role from the member
+                        else {
+                            guildController.removeSingleRoleFromMember(event.getMember(), role).queue();
+                            System.out.println("Removed " + role.getName() + " from " + event.getMember().getUser().getName());
+                            if (!Settings.getRoleRemovedEmoji().equals("")) {
+                                event.getChannel().addReactionById(event.getMessageId(), jda.getEmoteById(Settings.getRoleRemovedEmoji())).complete();
+                                event.getChannel().removeReactionById(event.getMessageId(), jda.getEmoteById(Settings.getRoleRemovedEmoji())).queueAfter(3, TimeUnit.SECONDS);
+                            }
+                        }
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Emoji not linked");
                     }
                 }
-                //Remove the role from the member
-                if (Commands.doesMemberHaveRole(event.getMember(), roleName)) {
-                    guildController.removeSingleRoleFromMember(event.getMember(), role).queue();
-                    System.out.println("Removed " + role.getName() + " from " + event.getMember().getUser().getName());
-                    if (!Settings.getRoleRemovedEmoji().equals("")) {
-                        event.getChannel().addReactionById(event.getMessageId(), jda.getEmoteById(Settings.getRoleRemovedEmoji())).complete();
-                        event.getChannel().removeReactionById(event.getMessageId(), jda.getEmoteById(Settings.getRoleRemovedEmoji())).queueAfter(3, TimeUnit.SECONDS);
-                    }
-                }
-            } catch (IllegalArgumentException e) {
-                System.out.println("Emoji not linked");
-            }
+            });
         }
-    }
-
-    public void refreshReactions(GuildMessageReactionAddEvent event, boolean force) {
-        event.getChannel().getMessageById(event.getMessageId()).queue((Message message) -> {
-            message.clearReactions().complete();
-            for (Emote emoji : message.getEmotes()) {
-                if (JsonDatabase.getLinkedRoleFromEmoji(emoji.getId()) != null)
-                    event.getChannel().addReactionById(event.getMessageId(), emoji).queue();            }
-
-        });
     }
 }
